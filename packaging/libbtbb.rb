1cf1ba495a2d196ae37284f01d95eb862a76f638
Dominic Spill <dominicgs@gmail.com>
2014-01-31 23:41:23 +0000

    Initial commit of homebrew formula

---
+++ packaging/libbtbb.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Libbtbb < Formula
+  homepage 'https://github.com/greatscottgadgets/libbtbb'
+  url 'https://github.com/greatscottgadgets/libbtbb/archive/2014-02-R1.tar.gz'
+  sha256 '91a0dafcb9911d6ca4959fdd8d2aaa5d2cfa76c9754757505898def08da7d5a3'
+
+  head 'https://github.com/greatscottgadgets/libbtbb.git'
+
+  option :universal
+
+  depends_on 'cmake' => :build
+
+  def install
+    if build.universal?
+      ENV.universal_binary
+      ENV['CMAKE_OSX_ARCHITECTURES'] = Hardware::CPU.universal_archs.as_cmake_arch_flags
+    end
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make install"
+    end
+  end
+end
