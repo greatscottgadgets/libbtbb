2704775a8ffc98a0abc0d8fd19378988db293572
Dominic Spill <dominicgs@gmail.com>
2014-02-19 22:08:49 +0000

    Update style to match homebrew style

--- packaging/libbtbb.rb
+++ packaging/libbtbb.rb
@@ -1,26 +1,26 @@
-require 'formula'
+require "formula"
 
 class Libbtbb < Formula
-  homepage 'https://github.com/greatscottgadgets/libbtbb'
-  url 'https://github.com/greatscottgadgets/libbtbb/archive/2014-02-R1.tar.gz'
-  sha256 '91a0dafcb9911d6ca4959fdd8d2aaa5d2cfa76c9754757505898def08da7d5a3'
-  version '2014-02-R1'
+  homepage "https://github.com/greatscottgadgets/libbtbb"
+  url "https://github.com/greatscottgadgets/libbtbb/archive/2014-02-R1.tar.gz"
+  sha1 "d0004f7d7afdee949cd7ddf0e3f13510861a9c7d"
+  version "2014-02-R1"
 
-  head 'https://github.com/greatscottgadgets/libbtbb.git'
+  head "https://github.com/greatscottgadgets/libbtbb.git"
 
   option :universal
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
   depends_on :python
 
   def install
     if build.universal?
       ENV.universal_binary
-      ENV['CMAKE_OSX_ARCHITECTURES'] = Hardware::CPU.universal_archs.as_cmake_arch_flags
+      ENV["CMAKE_OSX_ARCHITECTURES"] = Hardware::CPU.universal_archs.as_cmake_arch_flags
     end
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
-      system "make install"
+      system "make", "install"
     end
   end
 end
